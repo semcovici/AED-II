def31d8f93017f3c52c039602b5c3c74f5fbdb91
Pedro Semcovici <semcovici@pop-os.localdomain>
2022-06-23 16:22:03 -0300

    IV'

--- Listas/Exercícios básicos em arquivos/Arquivos/IV/main.cpp
+++ Listas/Exercícios básicos em arquivos/Arquivos/IV/main.cpp
@@ -1,3 +1,17 @@
+// IV-Serialização de EDs - faça você mesmo.
+
+// Passo 1: Escolha uma estrutura de dados dentre lista sequencial (i.e., vetor), lista ligada dinâmica
+// (simples ou dupla), árvore de busca binária, grafo em listas de adjacências ou grafo em matriz de
+// adjacências.
+
+// Passo 2: Escolha a operação de leitura (do arquivo para a memória) ou escrita (da ED em arquivo).
+// Tipicamente a leitura é mais trabalhosa, pois envolve recriar a ED usando malloc() etc.
+// Passo 3: Escolha o tipo de arquivo: texto com delimitadores (use fprintf/fscanf) ou binário (use
+// fwrite/fread).
+
+// Passo 4: Implemente o algoritmo correspondente à configuração escolhida (ED, operação, tipo de
+// arquivo)  e, em caso de dúvidas, teste no próprio compilador.
+
 // Passo 1: Grafo
 // Passo 2: escrita
 // Passo 3: binario
@@ -5,8 +19,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
-#define V 5
 
 typedef struct s
 {
@@ -20,13 +32,7 @@ typedef struct z
     NO *inicio;
 } VERTICE;
 
-typedef struct f
-{
-    NO *inicio;
-    NO *fim;
-} FILA;
-
-bool escrita(FILE* arq, VERTICE* g)
+bool escrita(FILE *arq, VERTICE *g, int V)
 {
     int t = sizeof(int);
 
@@ -50,3 +56,25 @@ bool escrita(FILE* arq, VERTICE* g)
     }
 }
 
+bool leitura(FILE *arq, VERTICE *g, int V) // vertice ja alocado e inicializado
+{
+    fread(&V, sizeof(int), 1, arq);
+
+    int origem, destino, peso;
+    while (1 == fread(&origem, sizeof(int), 1, arq))
+    {
+        fread(&destino, sizeof(int), 1, arq);
+        fread(&peso, sizeof(int), 1, arq);
+        inserirAresta(g, origem, destino, peso);
+    }
+}
+
+void inserirAresta(VERTICE *g, int origem, int destino, int peso)
+{
+
+    NO *novo = (NO *)malloc(sizeof(NO));
+    novo->peso = destino;
+    novo->peso = peso;
+    novo->prox = g[origem].inicio;
+    g[origem].inicio = novo;
+}
